dfc71aa2ea76a5e5fe60c2929dafe0bd349bb04d
mstanford <marks@reliablecoders.com>
2014-08-07 15:12:52 -0700

    Added App class

--- app/src/main/java/com/example/alex/mobackexperiment/MainActivity.java
+++ app/src/main/java/com/example/alex/mobackexperiment/MainActivity.java
@@ -7,14 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Arrays;
 import java.util.Random;
 
 
